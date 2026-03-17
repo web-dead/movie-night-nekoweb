1483351365555d52efa47eb83d96cab9af1224e7
web-dead <abitgrim@gmail.com>
2026-03-17 15:00:09 +1100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // DEPENDENCIES:
 const fs = require('fs');
-import * as core from "@actions/core";
+const core = require('@actions/core')
 // Read JSON file:
 const jsonFilePath = "public/movie-night-order.json";
 const fileData = fs.readFileSync(jsonFilePath, 'utf8');
