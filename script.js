4266f64bc2a6e3db8660ec63bb84ed77dad44a4c
web-dead <abitgrim@gmail.com>
2026-03-17 14:27:48 +1100

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 // DEPENDENCIES:
 const fs = require('fs');
-import * as core from "@actions/core";
-import * as github from "@actions/github";
+const core = require('@actions/core')
 // Read JSON file:
 const jsonFilePath = "public/movie-night-order.json";
 const fileData = fs.readFileSync(jsonFilePath, 'utf8');
